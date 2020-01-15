cc07c7990d83d6073ec11c757a15ed1d7a1a7c3f
Benjamin Wießneth <b.wiessneth@gmail.com>
2020-01-15 20:03:37 +0100

    Added files

---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.6
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/project_name')
+
+@app.route("/")
+def index():
+	message = "Hello from project_name"
+	return render_template('index.html', message=message)
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=1024, debug=True)
+
